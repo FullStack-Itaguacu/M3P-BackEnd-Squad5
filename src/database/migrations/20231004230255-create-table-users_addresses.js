fd8e8632f67cdbb3e763f446d563b2163c0f03df
William Monte de Moura <86812365+williammmoura@users.noreply.github.com>
2023-10-06 17:32:10 -0300

    Update src/database/migrations/20231004230255-create-table-users_addresses.js
    Co-authored-by: ERICO COUTO JR <63860659+EricoCoutoJr@users.noreply.github.com>

--- src/database/migrations/20231004230255-create-table-users_addresses.js
+++ src/database/migrations/20231004230255-create-table-users_addresses.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("users_addresses", {
+    await queryInterface.createTable("usersAddresses", {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
