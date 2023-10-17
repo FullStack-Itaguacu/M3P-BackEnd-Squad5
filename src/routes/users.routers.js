743ba83f1025d2a69776faffd9b600dd090977a8
douglas <douglas.df1@gmail.com>
2023-10-16 21:36:26 -0300

    feat: documentacao swagger

--- src/routes/users.routers.js
+++ src/routes/users.routers.js
@@ -1,18 +1,24 @@
-const { loginUser, createOneUser } = require('../controller/user.controller')
-const { Router } = require('express')
-const { auth } = require('../middleware/auth')
+const { loginUser, createOneUser } = require("../controller/user.controller");
+const { Router } = require("express");
+const { auth } = require("../middleware/auth");
 
-class UserRouter{
-    routesFromUser () {
-        const userRoutes  = Router()
-        userRoutes.post('/user/login', loginUser)
-        userRoutes.post('/user/signup', createOneUser)
-        userRoutes.post('/user/admin/signup', createOneUser)
-        // para as rotas privadas deve ser usada a autenticação
-        // deve ser considerado que apenas usuário adiministradores poderão usar a rota 
-        // '/user/admin/signup'
-        return userRoutes
-    }
+class UserRouter {
+  routesFromUser() {
+    const userRoutes = Router();
+    userRoutes.post("/user/login", loginUser /* #swagger.tags = ['Users']*/);
+    userRoutes.post(
+      "/user/signup",
+      createOneUser /* #swagger.tags = ['Users']*/
+    );
+    userRoutes.post(
+      "/user/admin/signup",
+      createOneUser /* #swagger.tags = ['Users']*/
+    );
+    // para as rotas privadas deve ser usada a autenticação
+    // deve ser considerado que apenas usuário adiministradores poderão usar a rota
+    // '/user/admin/signup'
+    return userRoutes;
+  }
 }
 
-module.exports = new UserRouter()
+module.exports = new UserRouter();
