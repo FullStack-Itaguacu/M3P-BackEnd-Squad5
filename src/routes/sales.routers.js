f7a695ec2ee2f44b857a796592f3552ca47dd96c
GabrielOmuro <gabriel_omuro@estudante.sesisenai.org.br>
2023-10-18 20:37:32 -0300

    Correção de erros.

--- src/routes/sales.routers.js
+++ src/routes/sales.routers.js
@@ -2,21 +2,20 @@ const { Router } = require("express");
 const {
   createSales,
   listSalesById,
+  dashboard,
 } = require("../controller/sales.controller");
 const { auth } = require("../middleware/auth");
 
 class SalesRouter {
   routesFromSales() {
     const salesRoutes = Router();
-    salesRoutes.post("/sales", createSales /* #swagger.tags = ['Sales']*/);
-    salesRoutes.get("/sales", listSalesById /* #swagger.tags = ['Sales']*/);
+    salesRoutes.post("/sales", auth, createSales /* #swagger.tags = ['Sales']*/);
+    salesRoutes.get("/sales", auth, listSalesById /* #swagger.tags = ['Sales']*/);
     salesRoutes.get(
       "/sales/admin",
-      listSalesById /* #swagger.tags = ['Sales']*/
+      auth, listSalesById /* #swagger.tags = ['Sales']*/
     );
-    salesRoutes.get(
-      "/sales/dashboard/admin",
-      listSalesById /* #swagger.tags = ['Sales']*/
+    salesRoutes.get("/sales/dashboard/admin", auth, dashboard /* #swagger.tags = ['Sales']*/
     );
     return salesRoutes;
   }
